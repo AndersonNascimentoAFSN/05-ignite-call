d61cf08c876918944aa6d605a5b61d7917359a27
Anderson Nascimento <anderson.nascimento@meta.com.br>
2022-10-19 17:33:53 -0300

    add Button and TextInput component

--- packages/react/src/components/Button.tsx
+++ packages/react/src/components/Button.tsx
@@ -2,29 +2,88 @@ import { ReactNode, ComponentProps, ElementType } from 'react'
 import { styled } from '../styles'
 
 export const Button = styled('button', {
-  fontFamily: '$default',
-  backgroundColor: '$ignite300',
+  all: 'unset',
   borderRadius: '$sm',
-  border: 0,
-  fontWeight: 'bold',
-  color: '$white',
+  fontSize: '$sm',
+  fontWeight: '$medium',
+  fontFamily: '$default',
+  textAlign: 'center',
+  minWidth: 120,
+  boxSizing: 'border-box',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: '$2',
+
   cursor: 'pointer',
 
+  svg: {
+    width: '$4',
+    height: '$4',
+  },
+
+  '&:disabled': {
+    cursor: 'not-allowed',
+  },
+
   variants: {
-    size: {
-      small: {
-        fontSize: 14,
-        padding: '$2 $4',
+    variant: {
+      primary: {
+        color: '$white',
+        background: '$ignite500',
+
+        '&:not(:disabled):hover': {
+          background: '$ignite300',
+        },
+        '&:disabled': {
+          backgroundColor: '$gray200',
+        },
       },
-      big: {
-        fontSize: 16,
-        padding: '$3 $6',
+      secondary: {
+        color: '$ignite300',
+        border: '2px solid $ignite500',
+
+        '&:not(:disabled):hover': {
+          background: '$ignite500',
+          color: '$white',
+        },
+        '&:disabled': {
+          color: '$gray200',
+          borderColor: '$gray200',
+        },
+      },
+      tertiary: {
+        color: '$gray100',
+
+        '&:not(:disabled):hover': {
+          color: '$white',
+        },
+        '&:disabled': {
+          color: '$gray600',
+        },
+      },
+    },
+
+    size: {
+      sm: {
+        padding: '$0',
+        paddingLeft: '$4',
+        paddingRight: '$4',
+        height: 38,
+      },
+      md: {
+        padding: '$0',
+        paddingLeft: '$4',
+        paddingRight: '$4',
+        height: 46,
       },
     },
   },
 
   defaultVariants: {
-    size: 'small',
+    variant: 'primary',
+    size: 'md',
   },
 })
 
