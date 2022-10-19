cde069dca7c3e35bf1430bc6e55ca5dedb10605c
Anderson Nascimento <anderson.nascimento@meta.com.br>
2022-10-19 11:41:37 -0300

    add avatar component

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,29 @@
+import { StoryObj, Meta } from '@storybook/react'
+
+import { Avatar, AvatarProps } from '@andersonnascimentoafsn-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/andersonnascimentoafsn.png',
+    alt: 'Anderson Nascimento',
+  },
+  argTypes: {},
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'Quando não for possível carregar a origem da imagem aparecerá um ícone em seu lugar',
+      },
+    },
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
