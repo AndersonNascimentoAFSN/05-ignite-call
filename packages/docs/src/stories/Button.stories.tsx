53b1d38e0a3c44866e8477b3624bad3a59d8561b
Anderson Nascimento <anderson.nascimento@meta.com.br>
2022-10-19 17:34:09 -0300

    add Button and TextInput component

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,29 +1,50 @@
 import { StoryObj, Meta } from '@storybook/react'
+import { ArrowRight } from 'phosphor-react'
 
 import { Button, ButtonProps } from '@andersonnascimentoafsn-ui/react'
 
 export default {
-  title: 'Components/Button',
+  title: 'Form/Button',
   component: Button,
   args: {
-    children: 'Click Here',
-    size: 'big',
+    children: 'Send',
+    variant: 'primary',
+    size: 'md',
+    disabled: false,
   },
   argTypes: {
     children: {
-      type: { name: 'string', required: true },
-      defaultValue: '',
+      // type: { name: 'string', required: true },
+      defaultValue: 'Send',
+    },
+    disabled: {
+      type: { name: 'boolean', required: false },
+      defaultValue: false,
+    },
+    variant: {
+      name: 'variants',
+      description: '',
+      defaultValue: 'primary',
+      type: { name: 'string', required: false },
+      table: {
+        type: { summary: 'primary, secondary or tertiary' },
+        defaultValue: { summary: 'primary' },
+      },
+      options: ['primary', 'secondary', 'tertiary'],
+      control: {
+        type: 'inline-radio',
+      },
     },
     size: {
       name: 'Sizes',
       description: 'Adjust size',
-      defaultValue: 'big',
+      defaultValue: 'md',
       type: { name: 'string', required: false },
       table: {
-        type: { summary: 'small or big' },
-        defaultValue: { summary: 'big' },
+        type: { summary: 'sm or md' },
+        defaultValue: { summary: 'md' },
       },
-      options: ['small', 'big'],
+      options: ['sm', 'md'],
       control: {
         type: 'inline-radio',
       },
@@ -32,3 +53,48 @@ export default {
 } as Meta<ButtonProps>
 
 export const Primary: StoryObj<ButtonProps> = {}
+
+export const Secondary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'secondary',
+    children: 'Create New',
+  },
+}
+
+export const Tertiary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'tertiary',
+    children: 'Cancel',
+  },
+}
+
+export const Small: StoryObj<ButtonProps> = {
+  args: {
+    size: 'sm',
+    children: 'Cancel',
+  },
+}
+
+export const WithIcon: StoryObj<ButtonProps> = {
+  args: {
+    children: (
+      <>
+        Próximo Passo
+        <ArrowRight weight="bold" />
+      </>
+    ),
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+}
+
+export const Disabled: StoryObj<ButtonProps> = {
+  args: {
+    disabled: true,
+  },
+}
